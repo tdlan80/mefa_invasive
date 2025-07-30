4edc9236be24884ca35c40e4ca2b2eb852b6ba38
mlammens <matt.lammens@gmail.com>
2025-07-30 15:06:30 -0400

    Adding a R script to use neonPlants functions

---
+++ code/misp_neonPlantEcology_use.R
@@ -0,0 +1,17 @@
+## Date originated: 2025-07-30
+## Date recent modification:
+##
+## Purpose: Attempting to use the neonPlants R package
+## to access the plant presence and percent cover data product
+
+## Load R packages
+
+## Install neonPlants
+# library(devtools)
+# devtools::install_github("NEONScience/neonPlants")
+
+library(pacman)
+library(neonPlants)
+
+pacman::p_load(tidyverse, neonUtilities)
+
